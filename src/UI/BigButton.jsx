661923c135c8fce5cf4897a86a511a0a550ac461
Артём <artemaa58092002@gmail.com>
2024-02-25 17:14:47 +0200

    Updates

--- src/UI/BigButton.jsx
+++
@@ -1,7 +0,0 @@
-const BigButton = ({ scrollToCatalog }) => {
-    return (
-        <button className="btn" onClick={scrollToCatalog}>Перейти к работе</button>
-    );
-}
- 
-export default BigButton;

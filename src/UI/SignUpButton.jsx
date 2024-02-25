c81872392f34e0f4851a26f9591f5973dc09f4fd
Артём <artemaa58092002@gmail.com>
2024-02-25 17:22:48 +0200

    new deploy1

--- src/UI/SignUpButton.jsx
+++ src/UI/SignUpButton.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const SignUp = () => {
   return (
     
-      <a href="/reg" className="registration">Создать аккаунт</a>
+      <a href="#/reg" className="registration">Создать аккаунт</a>
 
   );
 }

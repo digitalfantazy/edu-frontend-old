0911aa4fb82524c004f2fd3e124f4f75a6d52492
Артём <artemaa58092002@gmail.com>
2024-05-27 22:31:31 +0200

    Updates

--- src/modules/Catalog/index.js
+++
@@ -1,7 +0,0 @@
-import Catalog from "./components/Catalog";
-
-export { getPDF } from "./api/getPDF"
-export { E_Textbooks, OP, PT } from "./helpers/helpers";
-export { default as catalogSlice } from './store/catalogSlice';
-
-export default Catalog;

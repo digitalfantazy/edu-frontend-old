b5589402b31a4f97277900a5caf34d08fdda10fe
Артём <artemaa58092002@gmail.com>
2024-06-08 23:16:10 +0200

    Updates

--- src/modules/Student/index.js
+++
@@ -1,6 +0,0 @@
-import CardsContainer from "./components/CardsContainer";
-import PDFViewer from "./components/PDFViewer";
-
-export { default as literatureSlice } from "./store/literatureSlice";
-export { PDFViewer };
-export default CardsContainer;

68384b9174c29380fe15f7636bbebd3ac71a6b51
Артём <artemaa58092002@gmail.com>
2024-06-09 01:45:23 +0200

    changes

--- src/modules/Catalog/components/Labs/pdf.jsx
+++ src/modules/Catalog/components/Labs/pdf.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React from 'react';
+
 
 const GetPDFurl = ({ src, width, height, type }) => {
   return (
-    <div className="catalog-labs">
-      <object
-        data={`${src}#toolbar=0&navpanes=0&scrollbar=0`}
-        type="application/pdf"
-        width={width}
-        height={height}
-        aria-label="PDF Document"
-        title="pdf"
-      >
-        <p>Ваш браузер не поддерживает встраивание PDF. Вы можете скачать документ <a href={src}>здесь</a>.</p>
-      </object>
-    </div>
+      <div className="catalog-labs">
+        <object
+          data={`${src}#toolbar=0&navpanes=0&scrollbar=0`}
+          type="application/pdf"
+          width={width}
+          height={height}
+          aria-label="PDF Document"
+          title="pdf"
+        >
+          <p>Ваш браузер не поддерживает встраивание PDF. Вы можете скачать документ <a href={src}>здесь</a>.</p>
+        </object>
+      </div>
   );
 };
 
